67ae387779f79b90242d55ff2b69ce95c480a68c
Propeller <propeller@Propellers-Mac-mini.local>
2021-02-19 09:54:06 +0000

    Added loop code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  questionIndexes: Array<number> = [];
 
-  constructor() {}
+  constructor() {
+		this.questionIndexes = new Array(38);
+  }
 
 }
